d5212cc555efa979ae19a3dc51afa9b6034baa22
phuvinhnguyen <81418873+phuvinhnguyen@users.noreply.github.com>
2021-04-05 12:50:49 +0700

    Add files via upload
    file ko dùng string

--- Person.h
+++ Person.h
@@ -1,20 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 struct Person {
-private:
+protected:
     int id;
-    char* firstName, *lastName, *dob, *pass;
+    char* firstName,* lastName,* dob;
     bool gender;
 
 public:
-    Person(int id, char* firstName, char* lastName, char* dob, char* pass, bool gender) {
+    Person(int id, char* firstName, char* lastName, char* dob, bool gender) {
         this->id = id;
         this->firstName = firstName;
         this->lastName = lastName;
         this->dob = dob;
-        this->pass = pass;
         this->gender = gender;
     }
 
@@ -24,4 +23,24 @@ public:
         cout << "Day of birth: " << dob << '\n';
         cout << "Gender: " << (gender ? "Female" : "Male") << '\n';
     }
-};
+
+    int ID()
+    {
+        return id;
+    }
+
+    char* info(int input)
+    {
+        if (input == 1)
+            return firstName;
+        else if (input == 2)
+            return lastName;
+        else if (input == 3)
+            return dob;
+    }
+
+    bool gendr()
+    {
+        return gender;
+    }
+}; 
