e2d10edac7e465056976f49b2b99db70a54cf271
phuvinhnguyen <81418873+phuvinhnguyen@users.noreply.github.com>
2021-04-16 23:26:40 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include "main.h"
+#include "FileCVS.h"
+using namespace std;
 
-int main() {
-    
+int main()
+{
+    SignIn acc;
+    fstream fs("vidu.txt");
+    if(fs.fail())
+    {
+        ofstream ofs("vidu.txt");
+        ofs.close();
+        cout << "file vidu.txt chua co du lieu!!!";
+    }
+    fs.close();
+    //          account: 0 :enter        pass: hi :space
+    acc.LogIn("vidu.txt");
+    system("pause");
 }
