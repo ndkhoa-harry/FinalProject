9b3947f60292d161e886628b7dd46c763c4a8c41
phuvinhnguyen <81418873+phuvinhnguyen@users.noreply.github.com>
2021-04-19 15:27:50 +0700

    Add files via upload

--- Student.h
+++ Student.h
@@ -2,7 +2,7 @@
 #define _STUDENT_H_
 
 #include "Helper.h"
-
+#include <conio.h>
 #include "Person.h"
 
 class Student : public Person {
@@ -68,8 +68,31 @@ public:
 
     static Student* inputNewStudent(int no) {
         // TODO: Input new student by hand
+        int id, studentCount;
+        string firstName, lastName, dob;
+        char gender;
 
-        return nullptr;
+        cout << "Enter No. " << no << " student informations: \n";
+        cout << "\tID: "; cin >> id;
+
+        cout << "\tFirst Name: ";
+        cin.ignore();
+        getline(cin, firstName);
+
+        cout << "\tLast Name: ";
+        getline(cin, lastName);
+
+        cout << "\tGender (Male: Y/N): "; cin >> gender;
+        cout << "\tDate of birth (dd/mm/yyyy): "; cin >> dob;
+
+        return new Student(
+            no,
+            id,
+            firstName,
+            lastName,
+            gender == ('Y' || 'y') ? "Male" : "Female",
+            dob
+        );
     }
 
     static Student* getStudentFromStringStream(stringstream &s) {
