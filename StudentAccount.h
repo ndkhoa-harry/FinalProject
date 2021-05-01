c290166629052207c5f4e97204480e5375dccafe
Khoivcs <81419161+Khoivcs@users.noreply.github.com>
2021-05-01 21:10:55 +0700

    Update StudentAccount.h

--- StudentAccount.h
+++ StudentAccount.h
@@ -157,10 +157,43 @@ public:
 
     void getStudentAccountFromFile() {
         // TODO: Get student account informations from file
+         ifstream inp(STUDENTS_FILE + to_string(account -> getId()) + ".csv");
+        if (inp) {
+            string line, data;
+
+            getline(inp, schoolYearName, ',');
+            semester = SchoolYear::getLastedSemesterFromFile(schoolYearName);
+
+            getline(inp, className);
+
+            getline(inp, line);
+            stringstream s(line);
+            student = Student::getStudentFromStringStream(s);
+
+            while (getline(inp, line)) {
+                stringstream s(line);
+
+                getline(s, data, ',');
+                addCourse(semester -> getCourseFromID(stoi(data)));
+            }
+
+            inp.close();
     }
 
     void putStudentAccountToFile() {
         // TODO: Update student account informations to file
+        ofstream out(STUDENTS_FILE + to_string(account -> getId()) + ".csv");
+
+        out << schoolYearName << ',' << className << '\n';
+
+        student -> putDataToStream(out);
+
+        if (enrolledCoursesCount > 0)
+            out << enrolledCourses[0] -> getID();
+        for (int i = 1; i < enrolledCoursesCount; ++i)
+            out << ',' << enrolledCourses[i];
+
+        out.close();
     }
 };
 
