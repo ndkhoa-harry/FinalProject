9883985ca2a1cde4011cb9cadbcdc6d99bc72710
Nguyễn Đăng Khoa <43226890+NDKhoa892@users.noreply.github.com>
2021-05-01 18:57:03 +0700

    Update Picasso.h, add function drawBox

--- Picasso.h
+++ Picasso.h
@@ -11,12 +11,28 @@ using namespace std;
 const int WINDOW_HEIGHT = 32;
 const int WINDOW_WIDTH = 128;
 
-void initWindowSize() {
-    HWND console = GetConsoleWindow();
-    RECT r;
-    GetWindowRect(console, &r);
-    MoveWindow(console, r.left, r.top, 953, 550, TRUE); 
-}
+void initWindowSize() { 
+    COORD coord; 
+    coord.X = WINDOW_WIDTH; 
+    coord.Y = WINDOW_HEIGHT; 
+
+    SMALL_RECT Rect; 
+    Rect.Top = 0; 
+    Rect.Left = 0; 
+    Rect.Bottom = WINDOW_HEIGHT - 1; 
+    Rect.Right = WINDOW_WIDTH - 1; 
+
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle 
+    SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size 
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size 
+} 
+
+// void initWindowSize() {
+//     HWND console = GetConsoleWindow();
+//     RECT r;
+//     GetWindowRect(console, &r);
+//     MoveWindow(console, r.left, r.top, 953, 550, TRUE); 
+// }
 
 void gotoXY(short int x, short int y) {
     static HANDLE h = NULL;  
@@ -26,6 +42,7 @@ void gotoXY(short int x, short int y) {
 }
 
 /// DESIGN
+const int KEY_ARROW = 224;
 const int KEY_UP = 72;
 const int KEY_LEFT = 75;
 const int KEY_DOWN = 80;
@@ -33,32 +50,39 @@ const int KEY_RIGHT = 77;
 
 const int KEY_ENTER = 13;
 const int KEY_BACKSPACE = 8;
+const int KEY_CONTROL_V = 22;
 
 const char CHAR_VERTICAL = char(179);               // |
 const char CHAR_HORIZONTAL = char(196);             // -
 const char CHAR_VERTICAL_HORIZONTAL = char(197);    // +
+const char CHAR_VERTICAL_LEFT = char(180);          // -|
+const char CHAR_VERTICAL_RIGHT = char(195);         // |-
+const char CHAR_HORIZONTAL_DOWN = char(194);
+const char CHAR_HORIZONTAL_UP = char(193);
+const char CHAR_UPPER_LEFT = char(218);             
+const char CHAR_UPPER_RIGHT = char(191);
+const char CHAR_LOWER_LEFT = char(192);
+const char CHAR_LOWER_RIGHT = char(217);
 
-void drawMenu(int titleLines, const string* title, int optionsCount, const string* options, int &choice) {
-    system("cls");
-
-    int startX = (WINDOW_HEIGHT - titleLines - optionsCount * 3) / 2 - 1;
-    int x = startX;
-
-    if (titleLines == 0) {
-        startX -= 3;
-        x -= 3;
-    }
-
-    if (startX < 0) {
-        startX = 1;
-        x = 1;
-    }
-
+void drawTitle(int &x, int titleLines, const string* title) {
     for (int i = 0; i < titleLines; ++i) {
         gotoXY(x, (WINDOW_WIDTH - title[i].size()) / 2 - 1);
         cout << title[i];
         ++x;
     }
+}
+
+void drawMenu(int titleLines, const string* title, int optionsCount, const string* options, int &choice) {
+    system("cls");
+
+    int startX = (WINDOW_HEIGHT - titleLines - optionsCount * 3) / 2 - 1;
+
+    if (titleLines == 0) startX -= 3;
+    if (startX < 0) startX = 1;
+
+    int x = startX;
+
+    drawTitle(x, titleLines, title);
 
     for (int i = 0; i < optionsCount; ++i) {
         x += 3;
@@ -71,20 +95,34 @@ void drawMenu(int titleLines, const string* title, int optionsCount, const strin
         gotoXY(startX + titleLines + 3 + choice * 3, (WINDOW_WIDTH - options[choice].size()) / 2 - 4);
         cout << ">> " << options[choice] << " <<";
 
-        char c = getch();
+        gotoXY(WINDOW_HEIGHT - 1, 0);
+        cout << "Use arrow keys to move, enter to choose and backspace to return.";
+
+        int c = getch();
 
         gotoXY(startX + titleLines + 3 + choice * 3, (WINDOW_WIDTH - options[choice].size()) / 2 - 4);
         cout << "   " << options[choice] << "   ";
 
-        switch (int(c)) {
-            case KEY_UP:
-                choice = (choice + optionsCount - 1) % optionsCount;
+        switch (c) {
+            case KEY_ARROW: {
+                c = getch();
+
+                switch (c) {
+                    case KEY_DOWN:
+                        choice = (choice + 1) % optionsCount;
+                        break;
+                    
+                    case KEY_UP:
+                        choice = (choice + optionsCount - 1) % optionsCount;
+                        break;
+                }
+
                 break;
-            case KEY_DOWN:
-                choice = (choice + 1) % optionsCount;
-                break;
+            }
+
             case KEY_ENTER:
                 return;
+
             case KEY_BACKSPACE:
                 choice = -1;
                 return;
@@ -129,10 +167,6 @@ void drawTable(int startX, string tableTitle, int columnsCount, int* columnsWidt
     }
 
     ++x;
-    /*gotoXY(x, startY);
-    for (int i = 0; i < columnsWidthSum + columnsCount - 1; ++i) 
-        cout << '-';*/
-
     for (int i = 0; i < rowsCount; ++i) {
         ++x;
         y = startY;
@@ -152,6 +186,135 @@ void drawTable(int startX, string tableTitle, int columnsCount, int* columnsWidt
         }
 
         cout << '\n';
+    }
+}
+
+void drawBox(string title, int containsCount, string* contains, int optionsCount, string* options, int &choice) {
+    int width = title.size() + 4;
+
+    for (int i = 0; i < containsCount; ++i)
+        if (width < contains[i].size() + 4)
+            width = contains[i].size() + 4;
+
+    int optionsWidth = 0;
+    for (int i = 0; i < optionsCount; ++i) 
+        if (optionsWidth < options[i].size() + 6)
+            optionsWidth = options[i].size() + 6;
+
+    if (optionsWidth < (width - optionsCount - 1) / optionsCount + 1)
+        optionsWidth = (width - optionsCount - 1) / optionsCount + 1; 
+
+    if (width < optionsWidth * optionsCount + optionsCount + 1)
+        width = optionsWidth * optionsCount + optionsCount + 1;
+
+    int height = containsCount + 6;
+
+    int x = (WINDOW_HEIGHT - height) / 2 - 1;
+
+    int startY = (WINDOW_WIDTH - width) / 2 - 1;
+
+    for (int i = x; i <= x + height - 1; ++i) {
+        gotoXY(i, startY);
+        for (int j = 0; j < width; ++j) cout << ' ';
+    }
+    gotoXY(WINDOW_HEIGHT - 1, 0);
+    cout << "                                                                    ";
+
+    gotoXY(x, startY);
+    cout << CHAR_UPPER_LEFT;
+    for (int i = 2; i < width; ++i)
+        cout << CHAR_HORIZONTAL;
+    cout << CHAR_UPPER_RIGHT;
+
+    ++x;
+    gotoXY(x, startY);
+    cout << CHAR_VERTICAL << ' ' << title;
+    gotoXY(x, startY + width - 1);
+    cout << CHAR_VERTICAL;
+
+    ++x;
+    gotoXY(x, startY);
+    cout << CHAR_VERTICAL_RIGHT;
+    for (int i = 2; i < width; ++i)
+        cout << CHAR_HORIZONTAL;
+    cout << CHAR_VERTICAL_LEFT;
+
+    for (int i = 0; i < containsCount; ++i) {
+        ++x;
+        gotoXY(x, startY);
+        cout << CHAR_VERTICAL << ' ' << contains[i];
+        gotoXY(x, startY + width - 1);
+        cout << CHAR_VERTICAL;
+    }
+
+    ++x;
+    gotoXY(x, startY);
+    cout << CHAR_VERTICAL_RIGHT;
+    for (int i = 2; i < width; ++i)
+        cout << CHAR_HORIZONTAL;
+    cout << CHAR_VERTICAL_LEFT;
+
+    gotoXY(x + 2, startY);
+    cout << CHAR_LOWER_LEFT;
+    for (int i = 2; i < width; ++i)
+        cout << CHAR_HORIZONTAL;
+    cout << CHAR_LOWER_RIGHT;
+
+    ++x;
+    int y = startY;
+    gotoXY(x, y);
+    cout << CHAR_VERTICAL;
+    for (int i = 0; i < optionsCount; ++i) {
+        ++y;
+        gotoXY(x, y + (optionsWidth - options[i].size()) / 2);
+        cout << options[i];
+
+        y += optionsWidth;
+
+        gotoXY(x - 1, y);
+        if (i < optionsCount - 1)
+            cout << CHAR_HORIZONTAL_DOWN;
+
+        gotoXY(x, y);
+        cout << CHAR_VERTICAL;
+
+        gotoXY(x + 1, y);
+        if (i < optionsCount - 1)
+            cout << CHAR_HORIZONTAL_UP;
+    }
+
+    while (1) {
+        gotoXY(x, startY + choice * optionsWidth + choice + 1 + (optionsWidth - options[choice].size() - 4) / 2);
+        cout << "> " << options[choice] << " <";
+
+        gotoXY(WINDOW_HEIGHT - 1, 0);
+        cout << "Use arrow keys to move, enter to choose.";
+
+        int c = getch();
+
+        gotoXY(x, startY + choice * optionsWidth + choice + 1 + (optionsWidth - options[choice].size() - 4) / 2);
+        cout << "  " << options[choice] << "  ";
+
+        switch (c) {
+            case KEY_ARROW: {
+                c = getch();
+
+                switch (c) {
+                    case KEY_RIGHT:
+                        choice = (choice + 1) % optionsCount;
+                        break;
+                    
+                    case KEY_LEFT:
+                        choice = (choice + optionsCount - 1) % optionsCount;
+                        break;
+                }
+
+                break;
+            }
+
+            case KEY_ENTER:
+                return;
+        }
     }
 }
 
